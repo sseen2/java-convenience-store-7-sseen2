6230f37fdf1ffdfbff17ef394824dc98a1174b98
sseen2 <kse10023@naver.com>
2024-11-11 22:58:05 +0900

    test: 테스트 코드 추가

--- src/test/java/store/ApplicationTest.java
+++ src/test/java/store/ApplicationTest.java
@@ -38,10 +38,38 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 수량_변경_확인() {
+        assertSimpleTest(() -> {
+            run("[물-1]", "N", "Y", "[물-1]", "N", "N");
+            assertThat(output()).contains(
+                    "- 물 500원 10개",
+                    "- 물 500원 9개"
+            );
+        });
+    }
+
+    @Test
+    void 일반_상품_멤버십_할인() {
+        assertSimpleTest(() -> {
+            run("[물-2],[정식도시락-2]", "Y", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("멤버십할인-4,140");
+        });
+    }
+
+    @Test
     void 여러_개의_일반_상품_구매() {
         assertSimpleTest(() -> {
             run("[비타민워터-3],[물-2],[정식도시락-2]", "N", "N");
             assertThat(output().replaceAll("\\s", "")).contains("내실돈18,300");
+        });
+    }
+
+    @Test
+    void 프로모션_재고_확인() {
+        assertSimpleTest(() -> {
+            run("[콜라-5],[오렌지주스-2],[사이다-9]", "Y", "Y", "N", "N");
+            assertThat(output().replaceAll("\\s", ""))
+                    .contains("행사할인-5,800","내실돈12,800");
         });
     }
 
@@ -62,10 +90,74 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 형식_오류_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("컵라면-12", "N", "N");
+            assertThat(output()).contains("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 올바르지_않은_입력_예외_테스트() {
+        assertSimpleTest(() -> {
+            runException("[콜라-3]", "yes", "N");
+            assertThat(output()).contains("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
     void 재고에_없는_상품_주문() {
         assertSimpleTest(() -> {
             runException("[햄버거-1]", "N", "N");
             assertThat(output()).contains("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
+        });
+    }
+
+    @Test
+    void 프로모션_가능_재고보다_적게_주문_추가_출력() {
+        assertSimpleTest(() -> {
+            runException("[콜라-5]", "Y", "N");
+            assertThat(output()).contains("현재 콜라은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+        });
+    }
+
+    @Test
+    void 프로모션_가능_재고보다_적게_주문_추가_가격() {
+        assertSimpleTest(() -> {
+            runException("[콜라-5]", "N", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈4,000");
+        });
+    }
+
+    @Test
+    void 프로모션_가능_재고보다_적게_주문_추가_안할때_가격() {
+        assertSimpleTest(() -> {
+            runException("[콜라-8]", "N", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈6,000");
+        });
+    }
+
+    @Test
+    void 프로모션_가능_재고보다_많이_주문_삭제() {
+        assertSimpleTest(() -> {
+            runException("[콜라-11]", "Y", "N");
+            assertThat(output()).contains("현재 콜라 2개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
+        });
+    }
+
+    @Test
+    void 프로모션_가능_재고보다_많이_주문_삭제_가격() {
+        assertSimpleTest(() -> {
+            runException("[콜라-11]", "Y", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈8,000");
+        });
+    }
+
+    @Test
+    void 프로모션_가능_재고보다_많이_주문_삭제_안할때_가격() {
+        assertSimpleTest(() -> {
+            runException("[콜라-11]", "N", "N");
+            assertThat(output().replaceAll("\\s", "")).contains("내실돈6,000");
         });
     }
 
